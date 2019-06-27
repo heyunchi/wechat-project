8a6a1cd4e94fc41f1c62cc484617b7848a1de1bc
heyunchi <1029121385@qq.com>
2019-06-27 14:51:52 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        array: [{
+            target: '空气质量',
+            status: '很差',
+            message: '气象条件不利于空气污染物稀释、扩散和清除， 请尽量避免在室             外长时间活动。',
+        }, {
+            target: '舒适指数',
+            status: '舒适',
+            message: '白天虽然天气晴好，但早晚会感觉偏凉，午后舒适宜人'
+        }]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
